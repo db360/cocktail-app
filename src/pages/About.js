d8213b6ca3ff8dfcd7671f206a715f4576f5de34
David <da.b@hotmail.es>
2021-09-23 15:58:29 +0200

    cocktail final

---
+++ src/pages/About.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <section className="section about-section">
+      <h1 className="section-title">about us</h1>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nisi a officia at libero? Ad saepe ratione aliquam odio tempore, modi facilis reprehenderit unde id omnis deleniti obcaecati voluptate soluta harum vel repellat porro, rerum perferendis dignissimos nobis, officiis dolorem est!
+      </p>
+    </section>
+  )
+}
+
+export default About
